c560f0abdc271600e6cb99b0601092f41e1c5708
Evan Relf <evan@evanrelf.com>
2020-10-24 23:56:29 -0700

    Nix-ify

---
+++ default.nix
@@ -0,0 +1,38 @@
+let
+  haskellPackagesOverlay =
+    import ./nix/override-haskell-packages.nix {
+      packages = {
+        "evans-ghcid" = pkgs.nix-gitignore.gitignoreSource [ ./.nixignore ] ./.;
+      };
+      overrides = {
+        "relude" = oldCabal: {
+          patches = (oldCabal.patches or []) ++ [ ./nix/patches/relude.patch ];
+        };
+      };
+    };
+
+
+  pkgs = import ./nix/nixpkgs.nix { overlays = [ haskellPackagesOverlay ]; };
+
+
+  evans-ghcid = pkgs.haskellPackages.evans-ghcid;
+
+
+  executable = pkgs.haskell.lib.justStaticExecutables evans-ghcid;
+
+
+  shell =
+    evans-ghcid.env.overrideAttrs (old: {
+      buildInputs = with pkgs; old.buildInputs ++ [
+        cabal-install
+        ghcid
+      ];
+    });
+
+in
+  { inherit
+      evans-ghcid
+      executable
+      shell
+    ;
+  }
