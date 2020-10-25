d7dc735100b1276c38f78b8ddec231cb83dd8917
Evan Relf <evan@evanrelf.com>
2020-10-25 13:50:21 -0700

    Convert more 4 space indentation to 2 space

--- exe/Session.hs
+++ exe/Session.hs
@@ -47,17 +47,17 @@ import qualified System.Process as Process
 import qualified System.Time.Extra as System.Time
 
 data Session = Session
-    { ghci :: IORef (Maybe Ghci) -- ^ The Ghci session, or Nothing if there is none
-    , command :: IORef (Maybe (String, [String])) -- ^ The last command passed to sessionStart, setup operations
-    , warnings :: IORef [Load] -- ^ The warnings from the last load
-    , curdir :: IORef FilePath -- ^ The current working directory
-    , running :: Concurrent.Var Bool -- ^ Am I actively running an async command
-    , withThread :: Concurrent.ThreadId -- ^ Thread that called withSession
-    , allowEval :: Bool  -- ^ Is the allow-eval flag set?
-    }
+  { ghci :: IORef (Maybe Ghci) -- ^ The Ghci session, or Nothing if there is none
+  , command :: IORef (Maybe (String, [String])) -- ^ The last command passed to sessionStart, setup operations
+  , warnings :: IORef [Load] -- ^ The warnings from the last load
+  , curdir :: IORef FilePath -- ^ The current working directory
+  , running :: Concurrent.Var Bool -- ^ Am I actively running an async command
+  , withThread :: Concurrent.ThreadId -- ^ Thread that called withSession
+  , allowEval :: Bool  -- ^ Is the allow-eval flag set?
+  }
 
 enableEval :: Session -> Session
-enableEval s = s { allowEval = True }
+enableEval s = s{ allowEval = True }
 
 debugShutdown :: String -> IO ()
 debugShutdown x = when False $ print ("DEBUG SHUTDOWN" :: String, x)
@@ -67,38 +67,38 @@ debugShutdown x = when False $ print ("DEBUG SHUTDOWN" :: String, x)
 --   properly and any spawned Ghci processes will be aborted.
 withSession :: (Session -> IO a) -> IO a
 withSession f = do
-    ghci <- newIORef Nothing
-    command <- newIORef Nothing
-    warnings <- newIORef []
-    curdir <- newIORef "."
-    running <- Concurrent.newVar False
-    debugShutdown "Starting session"
-    withThread <- Concurrent.myThreadId
-    let allowEval = False
-    f Session{..} `Exception.finally` do
-        debugShutdown "Start finally"
-        Concurrent.modifyVar_ running $ const $ pure False
-        whenJustM (readIORef ghci) \v -> do
-            writeIORef ghci Nothing
-            debugShutdown "Calling kill"
-            kill v
-        debugShutdown "Finish finally"
+  ghci <- newIORef Nothing
+  command <- newIORef Nothing
+  warnings <- newIORef []
+  curdir <- newIORef "."
+  running <- Concurrent.newVar False
+  debugShutdown "Starting session"
+  withThread <- Concurrent.myThreadId
+  let allowEval = False
+  f Session{..} `Exception.finally` do
+    debugShutdown "Start finally"
+    Concurrent.modifyVar_ running $ const $ pure False
+    whenJustM (readIORef ghci) \v -> do
+      writeIORef ghci Nothing
+      debugShutdown "Calling kill"
+      kill v
+    debugShutdown "Finish finally"
 
 
 -- | Kill. Wait just long enough to ensure you've done the job, but not to see the results.
 kill :: Ghci -> IO ()
 kill ghci = Util.ignored do
-    void $ System.Time.timeout 5 do
-        debugShutdown "Before quit"
-        Util.ignored $ quit ghci
-        debugShutdown "After quit"
-    debugShutdown "Before terminateProcess"
-    Util.ignored $ Process.terminateProcess $ process ghci
-    debugShutdown "After terminateProcess"
-    -- Ctrl-C after a tests keeps the cursor hidden,
-    -- `setSGR []`didn't seem to be enough
-    -- See: https://github.com/ndmitchell/ghcid/issues/254
-    Ansi.showCursor
+  void $ System.Time.timeout 5 do
+    debugShutdown "Before quit"
+    Util.ignored $ quit ghci
+    debugShutdown "After quit"
+  debugShutdown "Before terminateProcess"
+  Util.ignored $ Process.terminateProcess $ process ghci
+  debugShutdown "After terminateProcess"
+  -- Ctrl-C after a tests keeps the cursor hidden,
+  -- `setSGR []`didn't seem to be enough
+  -- See: https://github.com/ndmitchell/ghcid/issues/254
+  Ansi.showCursor
 
 loadedModules :: [Load] -> [FilePath]
 loadedModules = List.nubOrd . map loadFile . filter (not . isLoadConfig)
@@ -110,44 +110,45 @@ qualify dir xs = [x{loadFile = dir </> loadFile x} | x <- xs]
 --   the list of files that were observed (both those loaded and those that failed to load).
 sessionStart :: Session -> String -> [String] -> IO ([Load], [FilePath])
 sessionStart Session{ ghci = ghciIORef, ..} cmd setup = do
-    Concurrent.modifyVar_ running $ const $ pure False
-    writeIORef command $ Just (cmd, setup)
+  Concurrent.modifyVar_ running $ const $ pure False
+  writeIORef command $ Just (cmd, setup)
 
-    -- cleanup any old instances
-    whenJustM (readIORef ghciIORef) \v -> do
-        writeIORef ghciIORef Nothing
-        void $ Concurrent.forkIO $ kill v
+  -- cleanup any old instances
+  whenJustM (readIORef ghciIORef) \v -> do
+    writeIORef ghciIORef Nothing
+    void $ Concurrent.forkIO $ kill v
 
-    -- start the new
-    Util.outStrLn $ "Loading " <> cmd <> " ..."
-    (v, messages0) <- Exception.mask \unmask -> do
-        (v, messages) <- unmask $ startGhci cmd Nothing $ const Util.outStrLn
-        writeIORef ghciIORef $ Just v
-        pure (v, messages)
+  -- start the new
+  Util.outStrLn $ "Loading " <> cmd <> " ..."
+  (v, messages0) <- Exception.mask \unmask -> do
+    (v, messages) <- unmask $ startGhci cmd Nothing $ const Util.outStrLn
+    writeIORef ghciIORef $ Just v
+    pure (v, messages)
 
-    -- do whatever preparation was requested
-    void $ exec v $ String.unlines setup
+  -- do whatever preparation was requested
+  void $ exec v $ String.unlines setup
 
-    -- deal with current directory
-    (dir, _) <- showPaths v
-    writeIORef curdir dir
-    let messages1 = qualify dir messages0
+  -- deal with current directory
+  (dir, _) <- showPaths v
+  writeIORef curdir dir
+  let messages1 = qualify dir messages0
 
-    let loaded = loadedModules messages1
-    evals <- performEvals v allowEval loaded
+  let loaded = loadedModules messages1
+  evals <- performEvals v allowEval loaded
 
-    -- install a handler
-    void $ Concurrent.forkIO do
-        code <- Process.waitForProcess $ process v
-        whenJustM (readIORef ghciIORef) \ghci ->
-            when (ghci == v) do
-                System.Time.sleep 0.3 -- give anyone reading from the stream a chance to throw first
-                Concurrent.throwTo withThread $ Exception.ErrorCall $ "Command \"" <> cmd <> "\" exited unexpectedly with " <> show code
+  -- install a handler
+  void $ Concurrent.forkIO do
+    code <- Process.waitForProcess $ process v
+    whenJustM (readIORef ghciIORef) \ghci ->
+      when (ghci == v) do
+        -- give anyone reading from the stream a chance to throw first
+        System.Time.sleep 0.3
+        Concurrent.throwTo withThread $ Exception.ErrorCall $ "Command \"" <> cmd <> "\" exited unexpectedly with " <> show code
 
-    -- handle what the process returned
-    let messages2 = mapMaybe tidyMessage messages1
-    writeIORef warnings $ getWarnings messages2
-    pure (messages2 <> evals, loaded)
+  -- handle what the process returned
+  let messages2 = mapMaybe tidyMessage messages1
+  writeIORef warnings $ getWarnings messages2
+  pure (messages2 <> evals, loaded)
 
 
 getWarnings :: [Load] -> [Load]
@@ -157,37 +158,37 @@ getWarnings messages = [m | m@Message{..} <- messages, loadSeverity == Warning]
 -- | Call 'sessionStart' at the previous command.
 sessionRestart :: Session -> IO ([Load], [FilePath])
 sessionRestart session@Session{..} = do
-    Just (cmd, setup) <- readIORef command
-    sessionStart session cmd setup
+  Just (cmd, setup) <- readIORef command
+  sessionStart session cmd setup
 
 
 performEvals :: Ghci -> Bool -> [FilePath] -> IO [Load]
 performEvals _ False _ = pure []
 performEvals ghci True reloaded = do
-    cmds <- mapM getCommands reloaded
-    join <$> forM cmds \(file, cmds') ->
-        forM cmds' \(num, cmd) -> do
-            ref <- newIORef []
-            execStream ghci cmd \_ resp -> modifyIORef ref (resp :)
-            resp <- String.unlines . reverse <$> readIORef ref
-            pure $ Eval $ EvalResult file (num, 1) cmd resp
+  cmds <- mapM getCommands reloaded
+  join <$> forM cmds \(file, cmds') ->
+    forM cmds' \(num, cmd) -> do
+      ref <- newIORef []
+      execStream ghci cmd \_ resp -> modifyIORef ref (resp :)
+      resp <- String.unlines . reverse <$> readIORef ref
+      pure $ Eval $ EvalResult file (num, 1) cmd resp
 
 
 getCommands :: FilePath -> IO (FilePath, [(Int, String)])
 getCommands fp = do
-    ls <- IO.readFileUTF8' fp
-    pure (fp, splitCommands $ List.zipFrom 1 $ String.lines ls)
+  ls <- IO.readFileUTF8' fp
+  pure (fp, splitCommands $ List.zipFrom 1 $ String.lines ls)
 
 splitCommands :: [(Int, String)] -> [(Int, String)]
 splitCommands [] = []
 splitCommands ((num, line) : ls)
-    | isCommand line =
-          let (cmds, xs) = List.span (isCommand . snd) ls
-           in (num, String.unwords $ fmap (drop $ length commandPrefix) $ line : fmap snd cmds) : splitCommands xs
-    | isMultilineCommandPrefix line =
-          let (cmds, xs) = break (isMultilineCommandSuffix . snd) ls
-           in (num, String.unlines (wrapGhciMultiline (fmap snd cmds))) : splitCommands (drop 1 xs)
-    | otherwise = splitCommands ls
+  | isCommand line =
+      let (cmds, xs) = List.span (isCommand . snd) ls
+       in (num, String.unwords $ fmap (drop $ length commandPrefix) $ line : fmap snd cmds) : splitCommands xs
+  | isMultilineCommandPrefix line =
+      let (cmds, xs) = break (isMultilineCommandSuffix . snd) ls
+       in (num, String.unlines (wrapGhciMultiline (fmap snd cmds))) : splitCommands (drop 1 xs)
+  | otherwise = splitCommands ls
 
 isCommand :: String -> Bool
 isCommand = isPrefixOf commandPrefix
@@ -215,31 +216,31 @@ wrapGhciMultiline xs = [":{"] <> xs <> [":}"]
 --   added back in.
 sessionReload :: Session -> IO ([Load], [FilePath], [FilePath])
 sessionReload session@Session{ ghci = ghciIORef, ..} = do
-    -- kill anything async, set stuck if you didn't succeed
-    old <- Concurrent.modifyVar running \b -> pure (False, b)
-    stuck <- if not old then pure False else do
-        Just ghci <- readIORef ghciIORef
-        fmap isNothing $ System.Time.timeout 5 $ interrupt ghci
+  -- kill anything async, set stuck if you didn't succeed
+  old <- Concurrent.modifyVar running \b -> pure (False, b)
+  stuck <- if not old then pure False else do
+    Just ghci <- readIORef ghciIORef
+    fmap isNothing $ System.Time.timeout 5 $ interrupt ghci
 
-    if stuck
-      then (\(messages,loaded) -> (messages,loaded,loaded)) <$> sessionRestart session
-      else do
-        -- actually reload
-        Just ghci <- readIORef ghciIORef
-        dir <- readIORef curdir
-        messages0 <- mapMaybe tidyMessage . qualify dir <$> reload ghci
-        loaded <- map ((dir </>) . snd) <$> showModules ghci
-        let reloaded = loadedModules messages0
-        warn <- readIORef warnings
-        evals <- performEvals ghci allowEval reloaded
+  if stuck then
+    (\(messages,loaded) -> (messages, loaded, loaded)) <$> sessionRestart session
+  else do
+    -- actually reload
+    Just ghci <- readIORef ghciIORef
+    dir <- readIORef curdir
+    messages0 <- mapMaybe tidyMessage . qualify dir <$> reload ghci
+    loaded <- map ((dir </>) . snd) <$> showModules ghci
+    let reloaded = loadedModules messages0
+    warn <- readIORef warnings
+    evals <- performEvals ghci allowEval reloaded
 
-        -- only keep old warnings from files that are still loaded, but did not reload
-        let validWarn w = loadFile w `elem` loaded && loadFile w `notElem` reloaded
-        -- newest warnings always go first, so the file you hit save on most recently has warnings first
-        let messages = messages0 <> filter validWarn warn
+    -- only keep old warnings from files that are still loaded, but did not reload
+    let validWarn w = loadFile w `elem` loaded && loadFile w `notElem` reloaded
+    -- newest warnings always go first, so the file you hit save on most recently has warnings first
+    let messages = messages0 <> filter validWarn warn
 
-        writeIORef warnings $ getWarnings messages
-        pure (messages <> evals, List.nubOrd (loaded <> reloaded), reloaded)
+    writeIORef warnings $ getWarnings messages
+    pure (messages <> evals, List.nubOrd (loaded <> reloaded), reloaded)
 
 
 -- | Run an exec operation asynchronously. Should not be a @:reload@ or similar.
@@ -247,27 +248,29 @@ sessionReload session@Session{ ghci = ghciIORef, ..} = do
 --   Argument to done is the final stderr line.
 sessionExecAsync :: Session -> String -> (String -> IO ()) -> IO ()
 sessionExecAsync Session{ ghci = ghciIORef, .. } cmd done = do
-    Just ghci <- readIORef ghciIORef
-    stderrIORef <- newIORef ""
-    Concurrent.modifyVar_ running $ const $ pure True
-    caller <- Concurrent.myThreadId
-    void $ flip Concurrent.forkFinally (either (Concurrent.throwTo caller) (const pass)) do
-        execStream ghci cmd \strm msg ->
-            when (msg /= "*** Exception: ExitSuccess") do
-                when (strm == Stderr) $ writeIORef stderrIORef msg
-                Util.outStrLn msg
-        old <- Concurrent.modifyVar running \b -> pure (False, b)
-        -- don't fire Done if someone interrupted us
-        stderr <- readIORef stderrIORef
-        when old $ done stderr
+  Just ghci <- readIORef ghciIORef
+  stderrIORef <- newIORef ""
+  Concurrent.modifyVar_ running $ const $ pure True
+  caller <- Concurrent.myThreadId
+  void $ flip Concurrent.forkFinally (either (Concurrent.throwTo caller) (const pass)) do
+    execStream ghci cmd \strm msg ->
+      when (msg /= "*** Exception: ExitSuccess") do
+        when (strm == Stderr) $ writeIORef stderrIORef msg
+        Util.outStrLn msg
+    old <- Concurrent.modifyVar running \b -> pure (False, b)
+    -- don't fire Done if someone interrupted us
+    stderr <- readIORef stderrIORef
+    when old $ done stderr
 
 
 -- | Ignore entirely pointless messages and remove unnecessary lines.
 tidyMessage :: Load -> Maybe Load
 tidyMessage Message{loadSeverity=Warning, loadMessage=[_,x]}
-    | Escape.unescape x == "    -O conflicts with --interactive; -O Util.ignored." = Nothing
+  | Escape.unescape x == "    -O conflicts with --interactive; -O Util.ignored." = Nothing
 tidyMessage m@Message{..}
-    = Just m{loadMessage = filter (\x -> not $ any (`isPrefixOf` Escape.unescape x) bad) loadMessage}
-    where bad = ["      except perhaps to import instances from"
-                ,"    To import instances alone, use: import "]
+  = Just m{loadMessage = filter (\x -> not $ any (`isPrefixOf` Escape.unescape x) bad) loadMessage}
+  where bad =
+          [ "      except perhaps to import instances from"
+          , "    To import instances alone, use: import "
+          ]
 tidyMessage x = Just x
--- exe/Wait.hs
+++ exe/Wait.hs
@@ -37,9 +37,9 @@ withWaiterPoll x f = f $ WaiterPoll x
 
 withWaiterNotify :: (Waiter -> IO a) -> IO a
 withWaiterNotify f = FsNotify.withManagerConf FsNotify.defaultConfig{FsNotify.confDebounce = FsNotify.NoDebounce} \manager -> do
-    mvar <- newEmptyMVar
-    var <- Concurrent.newVar Map.empty
-    f $ WaiterNotify manager mvar var
+  mvar <- newEmptyMVar
+  var <- Concurrent.newVar Map.empty
+  f $ WaiterNotify manager mvar var
 
 -- `listContentsInside test dir` will list files and directories inside `dir`,
 -- recursing into those subdirectories which pass `test`.
@@ -47,10 +47,10 @@ withWaiterNotify f = FsNotify.withManagerConf FsNotify.defaultConfig{FsNotify.co
 -- Subdirectories will have a trailing path separator, and are only listed if we recurse into them.
 listContentsInside :: (FilePath -> IO Bool) -> FilePath -> IO [FilePath]
 listContentsInside test dir = do
-    (dirs,files) <- Monad.partitionM Directory.doesDirectoryExist =<< Directory.listContents dir
-    recurse <- filterM test dirs
-    rest <- Monad.concatMapM (listContentsInside test) recurse
-    pure $ FilePath.addTrailingPathSeparator dir : files <> rest
+  (dirs,files) <- Monad.partitionM Directory.doesDirectoryExist =<< Directory.listContents dir
+  recurse <- filterM test dirs
+  rest <- Monad.concatMapM (listContentsInside test) recurse
+  pure $ FilePath.addTrailingPathSeparator dir : files <> rest
 
 -- | Given the pattern:
 --
@@ -64,65 +64,66 @@ listContentsInside test dir = do
 --   pures a message about why you are continuing (usually a file name).
 waitFiles :: Waiter -> IO ([(FilePath, a)] -> IO (Either String [(FilePath, a)]))
 waitFiles waiter = do
-    base <- getCurrentTime
-    pure (Exception.handle onError . go base)
- where
-    onError :: IOError -> IO (Either String [(FilePath, a)])
-    onError e = System.Time.sleep 1.0 >> pure (Left (show e))
+  base <- getCurrentTime
+  pure (Exception.handle onError . go base)
+  where
+  onError :: IOError -> IO (Either String [(FilePath, a)])
+  onError e = System.Time.sleep 1.0 >> pure (Left (show e))
 
-    go :: UTCTime -> [(FilePath, a)] -> IO (Either String [(FilePath, a)])
-    go base files = do
-        Verbosity.whenLoud $ Util.outStrLn $ "%WAITING: " <> String.unwords (map fst files)
-        -- As listContentsInside pures directories, we are waiting on them explicitly and so
-        -- will pick up new files, as creating a new file changes the containing directory's modtime.
-        files <- Monad.concatForM files \(file, a) ->
-            ifM (Directory.doesDirectoryExist file) (fmap (,a) <$> listContentsInside (pure . not . isPrefixOf "." . FilePath.takeFileName) file) (pure [(file, a)])
-        case waiter of
-            WaiterPoll _t -> pass
-            WaiterNotify manager kick mp -> do
-                dirs <- fmap Set.fromList $ mapM canonicalizePathSafe $ List.nubOrd $ map (FilePath.takeDirectory . fst) files
-                Concurrent.modifyVar_ mp \mp -> do
-                    let (keep,del) = Map.partitionWithKey (\k _v -> k `Set.member` dirs) mp
-                    sequence_ $ Map.elems del
-                    new <- forM (Set.toList $ dirs `Set.difference` Map.keysSet keep) \dir -> do
-                        can <- FsNotify.watchDir manager (fromString dir) (const True) \event -> do
-                            Verbosity.whenLoud $ Util.outStrLn $ "%NOTIFY: " <> show event
-                            void $ tryPutMVar kick ()
-                        pure (dir, can)
-                    let mp2 = keep `Map.union` Map.fromList new
-                    Verbosity.whenLoud $ Util.outStrLn $ "%WAITING: " <> String.unwords (Map.keys mp2)
-                    pure mp2
-                void $ tryTakeMVar kick
-        new <- mapM (Util.getModTime . fst) files
-        case [x | (x,Just t) <- zip files new, t > base] of
-            [] -> Right <$> recheck files new
-            xs -> pure (Right xs)
+  go :: UTCTime -> [(FilePath, a)] -> IO (Either String [(FilePath, a)])
+  go base files = do
+    Verbosity.whenLoud $ Util.outStrLn $ "%WAITING: " <> String.unwords (map fst files)
+    -- As listContentsInside pures directories, we are waiting on them explicitly and so
+    -- will pick up new files, as creating a new file changes the containing directory's modtime.
+    files <- Monad.concatForM files \(file, a) ->
+      ifM (Directory.doesDirectoryExist file) (fmap (,a) <$> listContentsInside (pure . not . isPrefixOf "." . FilePath.takeFileName) file) (pure [(file, a)])
+    case waiter of
+      WaiterPoll _t -> pass
+      WaiterNotify manager kick mp -> do
+        dirs <- fmap Set.fromList $ mapM canonicalizePathSafe $ List.nubOrd $ map (FilePath.takeDirectory . fst) files
+        Concurrent.modifyVar_ mp \mp -> do
+          let (keep,del) = Map.partitionWithKey (\k _v -> k `Set.member` dirs) mp
+          sequence_ $ Map.elems del
+          new <- forM (Set.toList $ dirs `Set.difference` Map.keysSet keep) \dir -> do
+            can <- FsNotify.watchDir manager (fromString dir) (const True) \event -> do
+              Verbosity.whenLoud $ Util.outStrLn $ "%NOTIFY: " <> show event
+              void $ tryPutMVar kick ()
+            pure (dir, can)
+          let mp2 = keep `Map.union` Map.fromList new
+          Verbosity.whenLoud $ Util.outStrLn $ "%WAITING: " <> String.unwords (Map.keys mp2)
+          pure mp2
+        void $ tryTakeMVar kick
+    new <- mapM (Util.getModTime . fst) files
+    case [x | (x,Just t) <- zip files new, t > base] of
+      [] -> Right <$> recheck files new
+      xs -> pure (Right xs)
 
-    recheck :: [(FilePath, a)] -> [Maybe UTCTime] -> IO [(String, a)]
-    recheck files old = do
-            System.Time.sleep 0.1
-            case waiter of
-                WaiterPoll t -> System.Time.sleep $ max 0 $ t - 0.1 -- subtract the initial 0.1 System.Time.sleep from above
-                WaiterNotify _ kick _ -> do
-                    takeMVar kick
-                    Verbosity.whenLoud $ Util.outStrLn "%WAITING: Notify signaled"
+  recheck :: [(FilePath, a)] -> [Maybe UTCTime] -> IO [(String, a)]
+  recheck files old = do
+    System.Time.sleep 0.1
+    case waiter of
+      WaiterPoll t -> System.Time.sleep $ max 0 $ t - 0.1 -- subtract the initial 0.1 System.Time.sleep from above
+      WaiterNotify _ kick _ -> do
+          takeMVar kick
+          Verbosity.whenLoud $ Util.outStrLn "%WAITING: Notify signaled"
+    new <- mapM (Util.getModTime . fst) files
+    case [x | (x,t1,t2) <- zip3 files old new, t1 /= t2] of
+      [] -> recheck files new
+      xs -> do
+        let disappeared = [x | (x, Just _, Nothing) <- zip3 files old new]
+        unless (null disappeared) do
+          -- if someone is deleting a needed file, give them some space to put the file back
+          -- typically caused by VIM
+          -- but try not to
+          Verbosity.whenLoud $ Util.outStrLn $ "%WAITING: Waiting max of 1s due to file removal, " <> String.unwords (List.nubOrd (map fst disappeared))
+          -- at most 20 iterations, but stop as soon as the file pures
+          void $ flip Monad.firstJustM (replicate 20 ()) \_ -> do
+            System.Time.sleep 0.05
             new <- mapM (Util.getModTime . fst) files
-            case [x | (x,t1,t2) <- zip3 files old new, t1 /= t2] of
-                [] -> recheck files new
-                xs -> do
-                    let disappeared = [x | (x, Just _, Nothing) <- zip3 files old new]
-                    unless (null disappeared) do
-                        -- if someone is deleting a needed file, give them some space to put the file back
-                        -- typically caused by VIM
-                        -- but try not to
-                        Verbosity.whenLoud $ Util.outStrLn $ "%WAITING: Waiting max of 1s due to file removal, " <> String.unwords (List.nubOrd (map fst disappeared))
-                        -- at most 20 iterations, but stop as soon as the file pures
-                        void $ flip Monad.firstJustM (replicate 20 ()) \_ -> do
-                            System.Time.sleep 0.05
-                            new <- mapM (Util.getModTime . fst) files
-                            pure $ if null [x | (x, Just _, Nothing) <- zip3 files old new] then Just () else Nothing
-                    pure xs
+            pure $ if null [x | (x, Just _, Nothing) <- zip3 files old new] then Just () else Nothing
+        pure xs
 
 
 canonicalizePathSafe :: FilePath -> IO FilePath
-canonicalizePathSafe x = Directory.canonicalizePath x `Exception.catch` \(_ :: IOError) -> pure x
+canonicalizePathSafe x =
+  Directory.canonicalizePath x `Exception.catch` \(_ :: IOError) -> pure x
